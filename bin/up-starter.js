9a7df781d1ec7e5c3b0cc905b3ebb2e0ae56e8ca
Gustavs T <fixplzsecrets@gmail.com>
2015-06-29 14:30:28 +0300

    Use ES async functions

--- bin/up-starter.js
+++ bin/up-starter.js
@@ -2,13 +2,14 @@
 
 var Up = require('..')
 
+process.argv.splice(1, 1)
+require(process.argv[1])
+
 Up.RPC.connect({
     name: 'Up-Node',
     attributes: { run: process.argv.slice(2) },
 })
 .then(function (client) {
-    process.argv.splice(1, 1)
-    require(process.argv[1])
     client.sendEvent('up')
 })
 .done()
