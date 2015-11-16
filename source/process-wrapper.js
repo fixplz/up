4519f7344dbef9326b8f2a3d9b215cacac3cd2e4
Gustavs T <fixplzsecrets@gmail.com>
2015-11-16 14:38:49 +0200

    Fix process wrapper arg list

--- source/process-wrapper.js
+++ source/process-wrapper.js
@@ -2,17 +2,16 @@ import Up from 'up'
 import Path from 'path'
 import {go} from 'up/util/async'
 
-process.argv.splice(1, 1)
-
-var target = Path.resolve(process.cwd(), process.argv[1])
 
 async () => {
     var client = await Up.RPC.connect({
         name: 'Up-Node',
-        attributes: { run: process.argv.slice(2) },
+        attributes: { run: process.argv.slice() },
     })
 
-    require(target)
+    process.argv.splice(1, 1)
+
+    require(Path.resolve(process.cwd(), process.argv[1]))
 
     return client.sendEvent('up')
 }().done()
